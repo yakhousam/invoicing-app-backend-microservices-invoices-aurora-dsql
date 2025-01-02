4b84735213aea3e60e42f383a5b8fec28fd0c17d
yakhousam <42283612+yakhousam@users.noreply.github.com>
2025-01-02 19:51:52 +0100

    feat: enhance invoice generation with update functionality and improved date handling

--- nodejs/src/tests/generate.ts
+++ nodejs/src/tests/generate.ts
@@ -1,4 +1,4 @@
-import { CreateInvoice, Invoice, Item } from '@/validation'
+import { CreateInvoice, Invoice, Item, UpdateInvoice } from '@/validation'
 import { faker } from '@faker-js/faker'
 
 export function generateCreateInvoice(): CreateInvoice {
@@ -24,7 +24,7 @@ export function generateInvoices(
   num: number,
   userId: string,
   userName: string
-): Invoice[] {
+): Omit<Invoice, 'status'>[] {
   const items: Item[] = Array.from(
     { length: faker.number.int({ min: 1, max: 10 }) },
     () => ({
@@ -44,16 +44,33 @@ export function generateInvoices(
     clientName: faker.company.name(),
     userId,
     userName,
-    invoiceDate: faker.date.recent().toISOString(),
+    invoiceDate: faker.date.recent({ days: 30 }).toISOString(),
     invoiceDueDays: faker.number.int({ min: 1, max: 30 }),
-    status: faker.helpers.arrayElement(['paid', 'sent', 'overdue']),
-    paid: faker.datatype.boolean(),
+    paid: false,
     subTotal: faker.number.float({ min: 0, max: 10000, fractionDigits: 2 }),
     taxAmount: faker.number.float({ min: 0, max: 3000, fractionDigits: 2 }),
     totalAmount: faker.number.float({ min: 0, max: 13000, fractionDigits: 2 }),
     createdAt: faker.date.past().toISOString(),
     updatedAt: faker.date.recent().toISOString()
   }))
+}
+
+export function generateUpdateInvoice(): UpdateInvoice {
+  return {
+    currency: faker.helpers.arrayElement(['USD', 'EUR', 'GBP']),
+    taxPercentage: faker.number.int({ min: 0, max: 30 }),
+    items: Array.from(
+      { length: faker.number.int({ min: 1, max: 10 }) },
+      () => ({
+        itemName: faker.commerce.productName(),
+        itemPrice: faker.number.float({ min: 0, max: 1000, fractionDigits: 2 }),
+        itemQuantity: faker.number.int({ min: 1, max: 100 })
+      })
+    ),
+    invoiceDate: maybe(faker.date.recent().toISOString()),
+    invoiceDueDays: maybe(faker.number.int({ min: 1, max: 30 })),
+    paid: true
+  }
 }
 
 export function generateUserId(): string {
