b57ce02786c06939a26fd08c15e388a4c82bc54f
yakhousam <42283612+yakhousam@users.noreply.github.com>
2025-01-03 15:47:33 +0100

    feat: refactor postInvoiceController to use invoice year for numbering and add status to response

--- nodejs/src/controllers/postInvoiceController.ts
+++ nodejs/src/controllers/postInvoiceController.ts
@@ -1,105 +1,92 @@
 import { ddbDocClient, tableName } from '@/db/client'
-import { createInvoiceSchema, invoiceSchema, type Invoice } from '@/validation'
+import { addStatusToInvoice } from '@/utils'
+import {
+  createInvoiceSchema,
+  invoiceSchema,
+  Item,
+  type Invoice
+} from '@/validation'
 import { PutCommand, UpdateCommand } from '@aws-sdk/lib-dynamodb'
 import {
   type APIGatewayProxyEvent,
   type APIGatewayProxyResult
 } from 'aws-lambda'
 import dayjs from 'dayjs'
-import createError from 'http-errors'
-import { z, ZodError } from 'zod'
+import { z } from 'zod'
 
 const postClientController = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  try {
-    const userId = invoiceSchema.shape.userId.parse(
-      event.requestContext.authorizer?.jwt?.claims?.sub
-    )
-    const userName = invoiceSchema.shape.userName.parse(
-      event.requestContext.authorizer?.jwt?.claims?.name
-    )
-    const validateBody = createInvoiceSchema.parse(event.body)
+  const userId = invoiceSchema.shape.userId.parse(
+    event.requestContext.authorizer?.jwt?.claims?.sub
+  )
+  const userName = invoiceSchema.shape.userName.parse(
+    event.requestContext.authorizer?.jwt?.claims?.name
+  )
+  const validateBody = createInvoiceSchema.parse(event.body)
 
-    const currentYear = dayjs().year()
+  const invoiceYear = dayjs(validateBody.invoiceDate).format('YYYY')
 
-    // Increment the invoice number for the current year
-    const updateCommand = new UpdateCommand({
-      TableName: tableName,
-      Key: { userId, invoiceId: `counter-${currentYear}` },
-      UpdateExpression:
-        'SET invoiceNumber = if_not_exists(invoiceNumber, :start) + :inc',
-      ExpressionAttributeValues: {
-        ':inc': 1,
-        ':start': 0
-      },
-      ReturnValues: 'UPDATED_NEW'
-    })
+  // Increment the invoice number for the current year
+  const updateCommand = new UpdateCommand({
+    TableName: tableName,
+    Key: { userId, invoiceId: `counter-${invoiceYear}` },
+    UpdateExpression:
+      'SET invoiceNumber = if_not_exists(invoiceNumber, :start) + :inc',
+    ExpressionAttributeValues: {
+      ':inc': 1,
+      ':start': 0
+    },
+    ReturnValues: 'UPDATED_NEW'
+  })
 
-    const counterResult = await ddbDocClient.send(updateCommand)
-    const incrementalNumber = z
-      .number()
-      .parse(counterResult.Attributes?.invoiceNumber)
+  const counterResult = await ddbDocClient.send(updateCommand)
+  const incrementalNumber = z
+    .number()
+    .parse(counterResult.Attributes?.invoiceNumber)
 
-    // Format the invoice number as "currentYear-incNumber"
-    const newInvoiceNumber = `${currentYear}-${incrementalNumber}`
+  // Format the invoice number as "invoiceYear-incNumber"
+  const newInvoiceNumber = `${invoiceYear}-${incrementalNumber}`
 
-    const subTotal = validateBody.items.reduce((acc, item) => {
-      return parseFloat((acc + item.itemPrice * item.itemQuantity).toFixed(2))
-    }, 0)
+  const subTotal = validateBody.items.reduce((acc, item) => {
+    return parseFloat((acc + item.itemPrice * item.itemQuantity).toFixed(2))
+  }, 0)
 
-    const taxAmount =
-      validateBody.taxPercentage > 0
-        ? parseFloat((subTotal * (validateBody.taxPercentage / 100)).toFixed(2))
-        : 0
-    const totalAmount = parseFloat((subTotal + taxAmount).toFixed(2))
+  const taxAmount =
+    validateBody.taxPercentage > 0
+      ? parseFloat((subTotal * (validateBody.taxPercentage / 100)).toFixed(2))
+      : 0
+  const totalAmount = parseFloat((subTotal + taxAmount).toFixed(2))
 
-    const newInvoice: Omit<Invoice, 'status'> = {
-      // status should be returned by the API but not saved in the database
-      ...validateBody,
-      invoiceId: newInvoiceNumber,
-      userId,
-      userName,
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString(),
-      items: validateBody.items.map((item) => ({
-        ...item,
-        itemId: crypto.randomUUID()
-      })) as [
-        {
-          itemId: string
-          itemName: string
-          itemPrice: number
-          itemQuantity: number
-        },
-        ...{
-          itemId: string
-          itemName: string
-          itemPrice: number
-          itemQuantity: number
-        }[]
-      ],
-      taxAmount,
-      subTotal,
-      totalAmount
-    }
+  const newInvoice: Omit<Invoice, 'status'> = {
+    // status should be returned by the API but not saved in the database
+    ...validateBody,
+    invoiceId: newInvoiceNumber,
+    userId,
+    userName,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    items: validateBody.items.map((item) => ({
+      ...item,
+      itemId: crypto.randomUUID()
+    })) as [Item, ...Item[]],
+    taxAmount,
+    subTotal,
+    totalAmount
+  }
 
-    const command = new PutCommand({
-      TableName: tableName,
-      Item: newInvoice
-    })
+  const command = new PutCommand({
+    TableName: tableName,
+    Item: newInvoice
+  })
 
-    await ddbDocClient.send(command)
+  await ddbDocClient.send(command)
 
-    return {
-      statusCode: 201,
-      body: JSON.stringify(newInvoice)
-    }
-  } catch (error) {
-    if (error instanceof ZodError) {
-      throw createError.BadRequest(error.message)
-    }
-    throw error
+  const returnInvoice = addStatusToInvoice(newInvoice)
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(returnInvoice)
   }
 }
 
--- nodejs/src/controllers/updateInvoiceController.ts
+++ nodejs/src/controllers/updateInvoiceController.ts
@@ -4,62 +4,47 @@ import {
   createUpdateExpression
 } from '@/utils'
 import { updateInvoiceSchema, invoiceSchema } from '@/validation'
-import { ConditionalCheckFailedException } from '@aws-sdk/client-dynamodb'
 import { UpdateCommand } from '@aws-sdk/lib-dynamodb'
 import {
   type APIGatewayProxyEvent,
   type APIGatewayProxyResult
 } from 'aws-lambda'
-import createError from 'http-errors'
-import { ZodError } from 'zod'
 
 const updateInvoiceController = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  try {
-    const userId = invoiceSchema.shape.userId.parse(
-      event.requestContext.authorizer?.jwt?.claims?.sub
-    )
+  const userId = invoiceSchema.shape.userId.parse(
+    event.requestContext.authorizer?.jwt?.claims?.sub
+  )
 
-    const invoiceId = event.pathParameters?.invoiceId
+  const invoiceId = event.pathParameters?.invoiceId
 
-    const updates = {
-      ...updateInvoiceSchema.parse(event.body),
-      updatedAt: new Date().toISOString()
-    }
+  const updates = {
+    ...updateInvoiceSchema.parse(event.body),
+    updatedAt: new Date().toISOString()
+  }
 
-    const updateExpression = createUpdateExpression(updates)
-    const expressionAttributeValues = createExpressionAttributeValues(updates)
+  const updateExpression = createUpdateExpression(updates)
+  const expressionAttributeValues = createExpressionAttributeValues(updates)
 
-    const command = new UpdateCommand({
-      TableName: tableName,
-      Key: {
-        invoiceId,
-        userId
-      },
-      UpdateExpression: updateExpression,
-      ExpressionAttributeValues: expressionAttributeValues,
-      ConditionExpression:
-        'attribute_exists(invoiceId) AND attribute_exists(userId)',
-      ReturnValues: 'ALL_NEW'
-    })
+  const command = new UpdateCommand({
+    TableName: tableName,
+    Key: {
+      invoiceId,
+      userId
+    },
+    UpdateExpression: updateExpression,
+    ExpressionAttributeValues: expressionAttributeValues,
+    ConditionExpression:
+      'attribute_exists(invoiceId) AND attribute_exists(userId)',
+    ReturnValues: 'ALL_NEW'
+  })
 
-    const result = await ddbDocClient.send(command)
+  const result = await ddbDocClient.send(command)
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result.Attributes)
-    }
-  } catch (error) {
-    if (error instanceof ZodError) {
-      throw createError.BadRequest(error.message)
-    }
-    if (error instanceof ConditionalCheckFailedException) {
-      throw createError.NotFound('Invoice not found')
-    }
-
-    console.error('Error instance:', error)
-    throw error
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result.Attributes)
   }
 }
 
