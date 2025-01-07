123ef1e8ae76d7e58beb50e80fd24d2cd14dc4ee
yakhousam <ykhoudir@gmail.com>
2025-01-07 18:16:58 +0100

    feat: enhance updateInvoiceController to include status in response and improve code formatting

--- nodejs/src/controllers/updateInvoiceController.ts
+++ nodejs/src/controllers/updateInvoiceController.ts
@@ -1,51 +1,54 @@
-import { ddbDocClient, tableName } from '@/db/client'
+import { ddbDocClient, tableName } from "@/db/client";
 import {
+  addStatusToInvoice,
   createExpressionAttributeValues,
-  createUpdateExpression
-} from '@/utils'
-import { updateInvoiceSchema, invoiceSchema } from '@/validation'
-import { UpdateCommand } from '@aws-sdk/lib-dynamodb'
+  createUpdateExpression,
+} from "@/utils";
+import { updateInvoiceSchema, invoiceSchema, Invoice } from "@/validation";
+import { UpdateCommand } from "@aws-sdk/lib-dynamodb";
 import {
   type APIGatewayProxyEvent,
-  type APIGatewayProxyResult
-} from 'aws-lambda'
+  type APIGatewayProxyResult,
+} from "aws-lambda";
 
 const updateInvoiceController = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   const userId = invoiceSchema.shape.userId.parse(
     event.requestContext.authorizer?.jwt?.claims?.sub
-  )
+  );
 
-  const invoiceId = event.pathParameters?.invoiceId
+  const invoiceId = event.pathParameters?.invoiceId;
 
   const updates = {
     ...updateInvoiceSchema.parse(event.body),
-    updatedAt: new Date().toISOString()
-  }
+    updatedAt: new Date().toISOString(),
+  };
 
-  const updateExpression = createUpdateExpression(updates)
-  const expressionAttributeValues = createExpressionAttributeValues(updates)
+  const updateExpression = createUpdateExpression(updates);
+  const expressionAttributeValues = createExpressionAttributeValues(updates);
 
   const command = new UpdateCommand({
     TableName: tableName,
     Key: {
       invoiceId,
-      userId
+      userId,
     },
     UpdateExpression: updateExpression,
     ExpressionAttributeValues: expressionAttributeValues,
     ConditionExpression:
-      'attribute_exists(invoiceId) AND attribute_exists(userId)',
-    ReturnValues: 'ALL_NEW'
-  })
+      "attribute_exists(invoiceId) AND attribute_exists(userId)",
+    ReturnValues: "ALL_NEW",
+  });
 
-  const result = await ddbDocClient.send(command)
+  const result = await ddbDocClient.send(command);
+
+  const returnInvoice = addStatusToInvoice(result.Attributes as Invoice);
 
   return {
     statusCode: 200,
-    body: JSON.stringify(result.Attributes)
-  }
-}
+    body: JSON.stringify(returnInvoice),
+  };
+};
 
-export default updateInvoiceController
+export default updateInvoiceController;
