8059009b1ddea2c55843c18b90c8a43e59f3d712
yakhousam <ykhoudir@gmail.com>
2025-01-22 12:52:35 +0100

    fix tests

--- nodejs/src/tests/delete-invoice.test.ts
+++ nodejs/src/tests/delete-invoice.test.ts
@@ -1,101 +1,102 @@
-import { DynamoDBDocumentClient, DeleteCommand } from '@aws-sdk/lib-dynamodb'
-import { type APIGatewayProxyEvent, type Context } from 'aws-lambda'
-import { mockClient } from 'aws-sdk-client-mock'
-import { beforeEach, describe, expect, it } from 'vitest'
-import { handler as deleteInvoicedHandler } from '../../functions/deleteInvoice'
-import { ConditionalCheckFailedException } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocumentClient, DeleteCommand } from "@aws-sdk/lib-dynamodb";
+import { type APIGatewayProxyEvent, type Context } from "aws-lambda";
+import { mockClient } from "aws-sdk-client-mock";
+import { beforeEach, describe, expect, it } from "vitest";
+import { handler as deleteInvoicedHandler } from "../../functions/deleteInvoice";
+import { ConditionalCheckFailedException } from "@aws-sdk/client-dynamodb";
+import { tableName } from "@/db/client";
 
-describe('Test deleteInvoice', () => {
-  const ddbMock = mockClient(DynamoDBDocumentClient)
+describe("Test deleteInvoice", () => {
+  const ddbMock = mockClient(DynamoDBDocumentClient);
 
   const event = {
-    httpMethod: 'DELETE',
+    httpMethod: "DELETE",
     headers: {
-      'cache-control': 'no-cache'
+      "cache-control": "no-cache",
     },
     requestContext: {
       authorizer: {
         jwt: {
           claims: {
-            sub: 'userId'
-          }
-        }
-      }
-    }
-  } as unknown as APIGatewayProxyEvent
+            sub: "userId",
+          },
+        },
+      },
+    },
+  } as unknown as APIGatewayProxyEvent;
 
   const context = {
-    getRemainingTimeInMillis: false
-  } as unknown as Context
+    getRemainingTimeInMillis: false,
+  } as unknown as Context;
 
   beforeEach(() => {
-    ddbMock.reset()
-  })
+    ddbMock.reset();
+  });
 
-  it('should delete a invoice by id', async () => {
-    const userId = 'userId'
-    const invoiceId = 'invoiceId'
+  it("should delete a invoice by id", async () => {
+    const userId = "userId";
+    const invoiceId = "invoiceId";
 
     ddbMock
       .on(DeleteCommand, {
-        TableName: 'invoices',
+        TableName: tableName,
         Key: {
           invoiceId: invoiceId,
-          userId
-        }
+          userId,
+        },
       })
-      .resolves({})
+      .resolves({});
 
     const deleteInvoiceEvent = {
       ...event,
       pathParameters: {
-        invoiceId
-      }
-    } as unknown as APIGatewayProxyEvent
+        invoiceId,
+      },
+    } as unknown as APIGatewayProxyEvent;
 
-    const response = await deleteInvoicedHandler(deleteInvoiceEvent, context)
+    const response = await deleteInvoicedHandler(deleteInvoiceEvent, context);
 
-    expect(response.statusCode).toBe(204)
-    expect(response.body).toBe(JSON.stringify({}))
-  })
+    expect(response.statusCode).toBe(204);
+    expect(response.body).toBe(JSON.stringify({}));
+  });
 
-  it('should return 404 if invoice not found', async () => {
-    const userId = 'userId'
-    const invoiceId = 'invoiceId'
+  it("should return 404 if invoice not found", async () => {
+    const userId = "userId";
+    const invoiceId = "invoiceId";
 
     ddbMock
       .on(DeleteCommand, {
-        TableName: 'invoices',
+        TableName: tableName,
         Key: {
           invoiceId: invoiceId,
-          userId
-        }
+          userId,
+        },
       })
       .rejects(
         new ConditionalCheckFailedException({
-          message: 'Invoice not found',
-          $metadata: {}
+          message: "Invoice not found",
+          $metadata: {},
         })
-      )
+      );
 
     const deleteInvoiceEvent = {
       ...event,
       pathParameters: {
-        invoiceId
-      }
-    } as unknown as APIGatewayProxyEvent
+        invoiceId,
+      },
+    } as unknown as APIGatewayProxyEvent;
 
-    const result = await deleteInvoicedHandler(deleteInvoiceEvent, context)
-    expect(result.statusCode).toBe(404)
-  })
-  it('should return 400 if invoiceId is not provided', async () => {
+    const result = await deleteInvoicedHandler(deleteInvoiceEvent, context);
+    expect(result.statusCode).toBe(404);
+  });
+  it("should return 400 if invoiceId is not provided", async () => {
     const deleteInvoiceEvent = {
       ...event,
-      pathParameters: {}
-    } as unknown as APIGatewayProxyEvent
+      pathParameters: {},
+    } as unknown as APIGatewayProxyEvent;
 
-    const result = await deleteInvoicedHandler(deleteInvoiceEvent, context)
-    expect(result.statusCode).toBe(400)
-    expect(result.body).toBe('invoiceId is required')
-  })
-})
+    const result = await deleteInvoicedHandler(deleteInvoiceEvent, context);
+    expect(result.statusCode).toBe(400);
+    expect(result.body).toBe("invoiceId is required");
+  });
+});
