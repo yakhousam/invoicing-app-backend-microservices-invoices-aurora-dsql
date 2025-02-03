08dd0fbe5161dff3f908b40cc68ff6852fc83d5f
yakhousam <ykhoudir@gmail.com>
2025-02-03 18:27:03 +0100

    feat: refactor getAllInvoicesController to use PostgreSQL for invoice retrieval and add pagination support

--- nodejs/src/controllers/getAllInvoicesController.ts
+++ nodejs/src/controllers/getAllInvoicesController.ts
@@ -1,55 +1,50 @@
-import { ddbDocClient, tableName } from '@/db/client'
-import { addStatusToInvoice } from '@/utils'
-import { Invoice, invoiceSchema } from '@/validation'
-import { QueryCommand } from '@aws-sdk/lib-dynamodb'
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import createError from 'http-errors'
+import { getDatabaseClient } from "@/db/client";
+import { addStatusToInvoice, getUserId } from "@/utils";
+import { Invoice, invoiceSchema, invoicesQuerySchema } from "@/validation";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import createError from "http-errors";
 
 const getAllInvoicesController = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const userId = invoiceSchema.shape.userId.parse(
-    event.requestContext.authorizer?.jwt?.claims?.sub
-  )
+  const userId = getUserId(event);
 
-  let lastEvaluatedKey: Record<string, unknown> | undefined = undefined
-  const invoices: Invoice[] = []
-
-  do {
-    const command: QueryCommand = new QueryCommand({
-      TableName: tableName,
-      ExclusiveStartKey: lastEvaluatedKey,
-      KeyConditionExpression: 'userId = :userId',
-      ExpressionAttributeValues: {
-        ':userId': userId
-      }
-    })
-    const data = await ddbDocClient.send(command)
-    const items = data.Items as Omit<Invoice, 'status'>[]
-    if (items) {
-      const filteredItems: Invoice[] = items
-        .filter((item) => !item.invoiceId.startsWith('counter'))
-        .map(addStatusToInvoice)
-
-      invoices.push(...filteredItems)
-    }
-    lastEvaluatedKey = data.LastEvaluatedKey
-  } while (lastEvaluatedKey)
-
-  if (invoices.length === 0) {
-    throw new createError.NotFound('No invoice found')
+  if (!userId) {
+    throw new createError.Unauthorized("Unauthorized");
   }
 
-  const parsedInvoices = invoiceSchema.array().parse(invoices)
+  const { limit, offset } = invoicesQuerySchema.parse(
+    event.queryStringParameters
+  );
+
+  const databaseClient = await getDatabaseClient();
+
+  const countResult = await databaseClient.query(
+    'SELECT COUNT(*) FROM invoicing_app.invoices WHERE "userId" = $1',
+    [userId]
+  );
+
+  const totalCount = parseInt(countResult.rows[0].count, 10);
+
+  const invoicesData = await databaseClient.query(
+    'SELECT * FROM invoicing_app.invoices WHERE "userId" = $1 LIMIT $2 OFFSET $3',
+    [userId, limit, offset]
+  );
+
+  const invoices = invoicesData.rows.map((invoice) =>
+    addStatusToInvoice(invoice as Invoice)
+  );
+
+  const parsedInvoices = invoiceSchema.array().parse(invoices);
 
   const response = {
     invoices: parsedInvoices,
-    count: invoices.length
-  }
+    count: totalCount,
+  };
   return {
     statusCode: 200,
-    body: JSON.stringify(response)
-  }
-}
+    body: JSON.stringify(response),
+  };
+};
 
-export default getAllInvoicesController
+export default getAllInvoicesController;
