5225e30f463888cb1ed20a5461715ab17f604f7c
yakhousam <ykhoudir@gmail.com>
2025-02-05 18:34:01 +0100

    feat: refactor utility functions for improved handling of updates and user authentication

--- nodejs/src/utils.ts
+++ nodejs/src/utils.ts
@@ -1,46 +1,47 @@
-import dayjs from 'dayjs'
-import { Invoice } from './validation'
+import dayjs from "dayjs";
+import { Invoice } from "./validation";
+
+import { APIGatewayProxyEvent } from "aws-lambda";
 
 export function createUpdateExpression(
   updates: Record<string, unknown>
 ): string {
-  return (
-    'SET ' +
-    Object.keys(updates)
-      .map((key) => `${key} = :${key}`)
-      .join(', ')
-  )
+  return Object.entries(updates)
+    .filter((update) => update[1] !== undefined)
+    .map(([key, value]) => `"${key}" = '${value}'`)
+    .join(", ");
 }
 
-export function createExpressionAttributeValues(
-  updates: Record<string, unknown>
-) {
-  return Object.entries(updates).reduce<Record<string, unknown>>(
-    (acc, [key, value]) => {
-      if (typeof value === 'object' && value !== null) {
-        acc[`:${key}`] = JSON.stringify(value)
-      } else {
-        acc[`:${key}`] = value
-      }
-      return acc
-    },
-    {}
-  )
+const isDev = process.env.NODE_ENV === "development";
+
+export function getUserId(event: APIGatewayProxyEvent): string {
+  return isDev
+    ? process.env.userId
+    : event.requestContext.authorizer?.jwt?.claims?.sub;
 }
 
-export function addStatusToInvoice(invoice: Omit<Invoice, 'status'>): Invoice {
-  let status: Invoice['status'] = 'sent'
+export function getAuthToken(event: APIGatewayProxyEvent): string {
+  if (isDev) {
+    return process.env.authToken as string;
+  }
+  const authHeader =
+    event.headers?.Authorization || event.headers?.authorization;
+  return authHeader?.replace("Bearer ", "") || "";
+}
+
+export function addStatusToInvoice(invoice: Omit<Invoice, "status">): Invoice {
+  let status: Invoice["status"] = "sent";
   if (invoice.paid) {
-    status = 'paid'
+    status = "paid";
   } else {
     status = dayjs().isAfter(
-      dayjs(invoice.invoiceDate).add(invoice.invoiceDueDays, 'day')
+      dayjs(invoice.invoiceDate).add(invoice.invoiceDueDays, "day")
     )
-      ? 'overdue'
-      : 'sent'
+      ? "overdue"
+      : "sent";
   }
   return {
     ...invoice,
-    status
-  }
+    status,
+  };
 }
