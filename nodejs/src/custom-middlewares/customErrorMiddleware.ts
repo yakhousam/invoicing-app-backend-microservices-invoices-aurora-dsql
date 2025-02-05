75b0cbedd3875aabd428d47dbcf1bd9dd52a0334
yakhousam <ykhoudir@gmail.com>
2025-02-05 18:35:37 +0100

    feat: refactor customErrorMiddleware to improve error handling and code consistency

--- nodejs/src/custom-middlewares/customErrorMiddleware.ts
+++ nodejs/src/custom-middlewares/customErrorMiddleware.ts
@@ -1,8 +1,7 @@
-import { ConditionalCheckFailedException } from '@aws-sdk/client-dynamodb'
-import middy from '@middy/core'
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import createError from 'http-errors'
-import { ZodError } from 'zod'
+import middy from "@middy/core";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import createError from "http-errors";
+import { ZodError } from "zod";
 
 const customErrorMiddleware = (): middy.MiddlewareObj<
   APIGatewayProxyEvent,
@@ -11,14 +10,10 @@ const customErrorMiddleware = (): middy.MiddlewareObj<
   return {
     onError: async (request) => {
       if (request.error instanceof ZodError) {
-        request.error = createError.BadRequest(request.error.message)
+        request.error = createError.BadRequest(request.error.message);
       }
+    },
+  };
+};
 
-      if (request.error instanceof ConditionalCheckFailedException) {
-        request.error = createError.NotFound()
-      }
-    }
-  }
-}
-
-export default customErrorMiddleware
+export default customErrorMiddleware;
