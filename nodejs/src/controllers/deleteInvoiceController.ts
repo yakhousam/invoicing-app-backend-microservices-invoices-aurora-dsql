6215fe2446bd120d31898d508a6f5d5a6f2b1556
yakhousam <ykhoudir@gmail.com>
2025-02-05 18:35:04 +0100

    feat: refactor deleteInvoiceController to use database client and improve error handling

--- nodejs/src/controllers/deleteInvoiceController.ts
+++ nodejs/src/controllers/deleteInvoiceController.ts
@@ -1,40 +1,44 @@
-import { ddbDocClient, tableName } from '@/db/client'
-import { invoiceSchema } from '@/validation'
-import { DeleteCommand } from '@aws-sdk/lib-dynamodb'
+import { getDatabaseClient } from "@/db/databaseClient";
+import { getUserId } from "@/utils";
 import {
   type APIGatewayProxyEvent,
-  type APIGatewayProxyResult
-} from 'aws-lambda'
-import createError from 'http-errors'
+  type APIGatewayProxyResult,
+} from "aws-lambda";
+import createError from "http-errors";
 
 const deleteInvoiceController = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const userId = invoiceSchema.shape.userId.parse(
-    event.requestContext.authorizer?.jwt?.claims?.sub
-  )
-  const invoiceId = event.pathParameters?.invoiceId
+  const databaseClient = await getDatabaseClient();
+  try {
+    const userId = getUserId(event);
+    const invoiceId = event.pathParameters?.invoiceId;
 
-  if (!invoiceId) {
-    throw new createError.BadRequest('invoiceId is required')
+    if (!invoiceId) {
+      throw new createError.BadRequest("invoiceId is required");
+    }
+
+    await databaseClient.query("BEGIN");
+
+    await databaseClient.query(
+      `DELETE FROM invoicing_app.items WHERE "invoiceId" = $1`,
+      [invoiceId]
+    );
+    await databaseClient.query(
+      `DELETE FROM invoicing_app.invoices WHERE "invoiceId" = $1 AND "userId" = $2`,
+      [invoiceId, userId]
+    );
+
+    await databaseClient.query("COMMIT");
+
+    return {
+      statusCode: 204,
+      body: JSON.stringify({}),
+    };
+  } catch (error) {
+    databaseClient.query("ROLLBACK");
+    throw error;
   }
+};
 
-  const command = new DeleteCommand({
-    TableName: tableName,
-    Key: {
-      invoiceId: invoiceId,
-      userId
-    },
-    ConditionExpression:
-      'attribute_exists(invoiceId) AND attribute_exists(userId)'
-  })
-
-  await ddbDocClient.send(command)
-
-  return {
-    statusCode: 204,
-    body: JSON.stringify({})
-  }
-}
-
-export default deleteInvoiceController
+export default deleteInvoiceController;
