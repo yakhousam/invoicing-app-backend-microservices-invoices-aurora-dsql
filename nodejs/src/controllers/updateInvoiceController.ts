8a191b2024d914bd9dc5513cd3aaf34999fa72d2
yakhousam <ykhoudir@gmail.com>
2025-02-05 18:35:32 +0100

    feat: enhance updateInvoiceController to calculate totals and manage invoice items with improved database interactions

--- nodejs/src/controllers/updateInvoiceController.ts
+++ nodejs/src/controllers/updateInvoiceController.ts
@@ -1,54 +1,95 @@
-import { ddbDocClient, tableName } from "@/db/client";
-import {
-  addStatusToInvoice,
-  createExpressionAttributeValues,
-  createUpdateExpression,
-} from "@/utils";
-import { updateInvoiceSchema, invoiceSchema, Invoice } from "@/validation";
-import { UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { getDatabaseClient } from "@/db/databaseClient";
+import { addStatusToInvoice, createUpdateExpression, getUserId } from "@/utils";
+import { Invoice, Item, updateInvoiceSchema } from "@/validation";
 import {
   type APIGatewayProxyEvent,
   type APIGatewayProxyResult,
 } from "aws-lambda";
+import createError from "http-errors";
 
 const updateInvoiceController = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const userId = invoiceSchema.shape.userId.parse(
-    event.requestContext.authorizer?.jwt?.claims?.sub
-  );
+  const databaseClient = await getDatabaseClient();
+  try {
+    const userId = getUserId(event);
 
-  const invoiceId = event.pathParameters?.invoiceId;
+    const invoiceId = event.pathParameters?.invoiceId;
 
-  const updates = {
-    ...updateInvoiceSchema.parse(event.body),
-    updatedAt: new Date().toISOString(),
-  };
+    const updates = {
+      ...updateInvoiceSchema.parse(event.body),
+      updatedAt: new Date().toISOString(),
+    };
 
-  const updateExpression = createUpdateExpression(updates);
-  const expressionAttributeValues = createExpressionAttributeValues(updates);
+    const { items, ...invoice } = updates;
 
-  const command = new UpdateCommand({
-    TableName: tableName,
-    Key: {
-      invoiceId,
-      userId,
-    },
-    UpdateExpression: updateExpression,
-    ExpressionAttributeValues: expressionAttributeValues,
-    ConditionExpression:
-      "attribute_exists(invoiceId) AND attribute_exists(userId)",
-    ReturnValues: "ALL_NEW",
-  });
+    const subTotal = items?.reduce((acc, item) => {
+      return parseFloat((acc + item.itemPrice * item.itemQuantity).toFixed(2));
+    }, 0);
 
-  const result = await ddbDocClient.send(command);
+    const taxAmount =
+      subTotal && invoice.taxPercentage && invoice.taxPercentage > 0
+        ? parseFloat((subTotal * (invoice.taxPercentage / 100)).toFixed(2))
+        : undefined;
 
-  const returnInvoice = addStatusToInvoice(result.Attributes as Invoice);
+    const totalAmount =
+      subTotal && taxAmount
+        ? parseFloat((subTotal + taxAmount).toFixed(2))
+        : undefined;
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(returnInvoice),
-  };
+    const updateExpression = createUpdateExpression({
+      ...invoice,
+      taxAmount,
+      subTotal,
+      totalAmount,
+    });
+
+    await databaseClient.query("BEGIN");
+
+    const updateInvoiceResult = await databaseClient.query(
+      `UPDATE invoicing_app.invoices SET ${updateExpression} WHERE "invoiceId" = $1 AND "userId" = $2 RETURNING *`,
+      [invoiceId, userId]
+    );
+
+    if (updateInvoiceResult.rowCount === 0) {
+      throw createError.NotFound(
+        `Invoice with invoiceId "${invoiceId}" not found`
+      );
+    }
+
+    if (items?.length) {
+      await databaseClient.query(
+        'DELETE FROM invoicing_app.items WHERE "invoiceId" = $1',
+        [invoiceId]
+      );
+      const itemsInsertPromises = items.map((item) =>
+        databaseClient.query(
+          `INSERT INTO invoicing_app.items ( "invoiceId", "itemName", "itemPrice", "itemQuantity") VALUES ($1, $2, $3, $4) RETURNING *`,
+          [invoiceId, item.itemName, item.itemPrice, item.itemQuantity]
+        )
+      );
+      await Promise.all(itemsInsertPromises);
+    }
+
+    const itemsResult = await databaseClient.query(
+      `SELECT * FROM invoicing_app.items WHERE "invoiceId" = $1`,
+      [invoiceId]
+    );
+    await databaseClient.query("COMMIT");
+
+    const returnInvoice = {
+      ...addStatusToInvoice(updateInvoiceResult.rows?.[0] as Invoice),
+      items: itemsResult.rows as Item[],
+    };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(returnInvoice),
+    };
+  } catch (error) {
+    await databaseClient.query("ROLLBACK");
+    throw error;
+  }
 };
 
 export default updateInvoiceController;
